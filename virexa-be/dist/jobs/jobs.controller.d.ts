e920d4307b5d9e693750c36287239209eaf99045
YourGitHubName <your@email.com>
2026-09-17 06:05:09 -0400

    seperate frontend and backend from same repository

--- virexa-be/dist/jobs/jobs.controller.d.ts
+++
@@ -1,7 +0,0 @@
-import { JobsService } from './jobs.service';
-export declare class JobsController {
-    private readonly jobsService;
-    constructor(jobsService: JobsService);
-    getJobs(): Promise<import("./job.entity").Job[]>;
-    getJobBySlug(slug: string): Promise<import("./job.entity").Job | null>;
-}
--- virexa-be/dist/users/users.service.d.ts
+++
@@ -1,8 +0,0 @@
-import { Repository } from 'typeorm';
-import { User } from './user.entity';
-export declare class UsersService {
-    private readonly userRepository;
-    constructor(userRepository: Repository<User>);
-    findByEmail(email: string): Promise<User | null>;
-    create(data: Partial<User>): Promise<Partial<User> & User>;
-}
